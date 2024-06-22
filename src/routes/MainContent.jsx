fc4eb59419d59dca75ca49c3e6fe1188d2be17fe
LuizFigueiredo <figueiredocabralluizcarlos@gmail.com>
2024-06-22 14:08:18 -0300

    :bug: fix: Bug nas rotas

--- src/routes/MainContent.jsx
+++ src/routes/MainContent.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import Home from '../pages/Home';
-import Footer from '../components/navbar e footer/Footer';
-import { About } from '../pages/About';
-import { Tech } from '../pages/Tech';
-import { Project } from '../pages/Projects';
-import { Contato } from '../pages/Contato';
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "../pages/Home";
+import Footer from "../components/navbar e footer/Footer";
+import { About } from "../pages/About";
+import { Tech } from "../pages/Tech";
+import { Project } from "../pages/Projects";
+import { Contato } from "../pages/Contato";
 
 const MainContent = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <>
-            <Routes>
-                <Route path='/home' element={<Home />} />
-                <Route path='/sobre' element={<About />} />
-                <Route path='/stacks' element={<Tech />} />
-                <Route path='/projetos' element={<Project />} />
-                <Route path='/contato' element={<Contato />} />
-            </Routes>
-            {location.pathname === '/home' && <Footer />}
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/stacks" element={<Tech />} />
+        <Route path="/projetos" element={<Project />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+      {location.pathname === "/home" && <Footer />}
+    </>
+  );
 };
 
 export default MainContent;
