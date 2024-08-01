abb4bdc91467793853f9ca171b01b01cbeae2aaa
LuizFigueiredo <figueiredocabralluizcarlos@gmail.com>
2024-08-01 08:38:23 -0300

    fix

--- src/components/navbar e footer/NavBar.jsx
+++ src/components/navbar e footer/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./NavBar.module.css";
 import Logo from "../../assets/logo2.svg";
 import Git from "../../assets/gitCard.svg";
@@ -6,30 +7,44 @@ import Instagram from "../../assets/instagram.svg";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <header className={styles.HeaderNav}>
       <nav className={styles.Navbar}>
         <div className={styles.Logo}>
-          <img src={Logo}></img>
+          <img src={Logo} alt="Logo" />
         </div>
-        <div className={styles.DivLista}>
+
+        {/* Botão Hamburger */}
+        <button
+          className={`${styles.burger} ${isMenuOpen ? "open" : ""}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        {/* Menu (visível em telas maiores ou quando o menu estiver aberto) */}
+        <div className={`${styles.DivLista} ${isMenuOpen ? styles.open : ""}`}>
           <div className={styles.Nav}>
             <ul className={styles.Lista}>
-              <Link to="/home">
-                <li>Home</li>
-              </Link>
-              <Link to="/sobre">
-                <li>Sobre</li>
-              </Link>
-              <Link to="/stacks">
-                <li>Tecnologias</li>
-              </Link>
-              <Link to="/projetos">
-                <li>Projetos</li>
-              </Link>
-              <Link to="/contato">
-                <li>Contato</li>
-              </Link>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/sobre">Sobre</Link>
+              </li>
+              <li>
+                <Link to="/stacks">Tecnologias</Link>
+              </li>
+              <li>
+                <Link to="/projetos">Projetos</Link>
+              </li>
+              <li>
+                <Link to="/contato">Contato</Link>
+              </li>
             </ul>
           </div>
           <div className={styles.redes}>
@@ -38,21 +53,21 @@ function NavBar() {
               rel="noopener noreferrer"
               href="https://github.com/LuizCFigueiredo"
             >
-              <img id="imgs" src={Git} alt="Meu GitHub"></img>
+              <img id="imgs" src={Git} alt="Meu GitHub" />
             </a>
             <a
               target="_blank"
               rel="noopener noreferrer"
               href="https://www.instagram.com/luizc.f.c/"
             >
-              <img id="imgs" src={Instagram} alt="Meu Instagram"></img>
+              <img id="imgs" src={Instagram} alt="Meu Instagram" />
             </a>
             <a
               target="_blank"
               rel="noopener noreferrer"
               href="https://www.linkedin.com/in/figueiredo-luiz/"
             >
-              <img id="imgs" src={Linkedin} alt="Meu Linkedin"></img>
+              <img id="imgs" src={Linkedin} alt="Meu Linkedin" />
             </a>
           </div>
         </div>
